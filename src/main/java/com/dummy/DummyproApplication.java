2db80ffa9bb432029854ded9edc8413600b8d310
govindu117 <govindugandha12472@gmail.com>
2025-06-08 11:13:40 +0530

    first-commit

---
+++ src/main/java/com/dummy/DummyproApplication.java
@@ -0,0 +1,13 @@
+package com.dummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyproApplication.class, args);
+	}
+
+}
